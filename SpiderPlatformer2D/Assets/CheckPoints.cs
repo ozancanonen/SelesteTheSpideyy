50e135f2b9ee1f55d6d15a21ea8a7d842d5d6d3b
UgurArdin <ugurardin@gmail.com>
2020-06-23 22:43:34 +0300

    Save Point artık çalışıyor amına koyim

--- SpiderPlatformer2D/Assets/CheckPoints.cs
+++ SpiderPlatformer2D/Assets/CheckPoints.cs
@@ -8,28 +8,18 @@ public class CheckPoints : MonoBehaviour
     [SerializeField] GameObject foregroundParallaxObjects;
     private bool ifCame;
 
-    private void Awake()
-    {
-        ifCame = false;
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Player")&& !ifCame)
+        if(collision.CompareTag("Player"))
         {
-            ifCame = true;
             PlayerPrefsController.cameraChanged = true;
             PlayerPrefsController.Instance.ChangeCameraIndex(currentCameraIndex);
-            //PlayerPrefsController.Instance.ChangeCameraState();
             PlayerPrefsController.Instance.SavePosition(transform.position.x, transform.position.y);
-            var wormlingHealth = FindObjectsOfType<WormlingHealth>();
-            if(wormlingHealth!=null)
+            if(!ifCame)
             {
-                foreach(WormlingHealth wormling in wormlingHealth)
-                {
-                    wormling.RestoreHealth();
-                }
+                ifCame = true;
+                collision.gameObject.GetComponent<PlayerController>().GetHealed();
             }
-            collision.gameObject.GetComponent<PlayerController>().GetHealed();
             plantAnimator.SetBool("Open", true);
 
             if(foregroundParallaxObjects!=null&&(currentCameraIndex == 1|| currentCameraIndex == 2|| currentCameraIndex == 3))
