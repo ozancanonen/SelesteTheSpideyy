fe245d58106f485d87e1befd240ed21760814033
emre-glitch <65126319+emre-gltich@users.noreply.github.com>
2020-06-24 07:29:05 +0300

    creditss

---
+++ SpiderPlatformer2D/Assets/Gotomainmenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gotomainmenu : MonoBehaviour
+{
+    [SerializeField]float time;
+    private void Start()
+    {
+        Invoke("goToMainMenu", time);
+    }
+
+private void goToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
