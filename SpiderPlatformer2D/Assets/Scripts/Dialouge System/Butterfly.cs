068e7fda9002269d063a1a24c2577d9b0114623f
ozancanonen <51087802+ozancanonen@users.noreply.github.com>
2020-07-19 18:04:45 +0300

    Organising

---
+++ SpiderPlatformer2D/Assets/Scripts/Dialouge System/Butterfly.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Butterfly : MonoBehaviour, INPC
+{
+    public int playersCame { get; set; } = 0;
+    bool playerCompleted = false;
+    public static bool npcEnder;
+    public static int childCount = 0;
+    public int cameraIndex = 1;
+    [SerializeField] GameObject glideObtainObject;
+    [SerializeField] Vector3 glideObtainObjectForce;
+    [SerializeField] Transform glideObtainObjectSpawnPos;
+    bool ifThrowedGlide;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (npcEnder == true) { return; }
+        Debug.Log("player came");
+        playersCame++;
+        if (collision.gameObject.tag == "Player")
+        {
+            StartCoroutine(cameraChangeAfter());
+            GetComponent<DialogueTrigger>().TriggerDialogue(playerCompleted, playersCame);
+
+            StartCoroutine(giveGlideSkill());
+        }
+    }
+
+    IEnumerator cameraChangeAfter()
+    {
+        yield return new WaitForSeconds(2f);
+        PlayerPrefsController.Instance.ChangeCameraIndex(cameraIndex);
+        //oldVirtualCamera.SetActive(false);
+        //newVirtualCamera.SetActive(true);
+    }
+    public void QuestCompleted()
+    {
+        playerCompleted = true;
+        GetComponent<Animator>().SetTrigger("HappyIdle");
+        Web_Projectile.canWeb = true;
+    }
+    IEnumerator giveGlideSkill()
+    {
+        ifThrowedGlide = true;
+        yield return new WaitForSeconds(3f);
+        var webObtain = Instantiate(glideObtainObject, glideObtainObjectSpawnPos.position, Quaternion.identity);
+        webObtain.GetComponent<Rigidbody2D>().AddForce(glideObtainObjectForce);
+    }
+
+}
---
+++ SpiderPlatformer2D/Assets/Scripts/Enemies/EnemyPatrolling.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrolling : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+    public float speed = 10f;
+    BoxCollider2D myFeet;
+    [SerializeField] GameObject hitParticle;
+    [SerializeField] Animator patrolAntAnim;
+    [SerializeField] BoxCollider2D dieCollider;
+    [SerializeField] BoxCollider2D bodyCollider;
+    bool isDead;
+    void Start()
+    {
+        myFeet = GetComponent<BoxCollider2D>();
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.velocity = new Vector3(speed, 0, 0);
+
+    }
+
+    // Update is called once per frame
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!isDead&&collision.gameObject.tag == "Grappable" || collision.gameObject.tag == "Pullable")
+            SetSpeed();
+        if (!isDead && collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerController>().UpdateHealth(10);
+            ContactPoint2D contact = collision.contacts[0];
+            var particle = Instantiate(hitParticle, contact.point, Quaternion.identity);
+            Destroy(particle, 1f);
+        }
+    }
+   
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (myFeet.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+        SetSpeed();
+    }
+  
+    private void SetSpeed()
+    {
+        speed = speed * -1;
+        rigidbody.velocity = new Vector3(speed, 0, 0);
+        FlipSprite();
+    }
+
+    private void FlipSprite()
+    {
+        transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+    }
+    public void Die()
+    {
+        GetComponentInChildren<PatrolBombs>().InstantiateBombs();
+        isDead = true;
+        patrolAntAnim.SetTrigger("getSquashed");
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.gravityScale = 0;
+        dieCollider.enabled = false;
+        bodyCollider.enabled = false;
+        Destroy(gameObject,2);
+    }
+   
+}
